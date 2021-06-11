deeb2b169f3e2002bd32191a1ad415b914b2783e
Joa1Gomes <joao.gomes181@etec.sp.gov.br>
2021-06-11 17:28:23 -0300

    Continuação

--- funcoes.js
+++ funcoes.js
@@ -7,12 +7,41 @@ function operacao(){
     altura = document.getElementById("altura").value;
 
     if(nome=="" || idade==""||peso==""||altura==""){
-        document.getElementById('erro').style.display = "block";
-    
-    } else{
-        imc = peso/ (altura*altura);
+        alert("Preencha os espacos em branco")
+    }else {
+        imc = peso/(altura*altura);
+
+     if (imc < 18.5){
+        estado = "Peso Baixo"
+    }else if(imc < 24.9){
+        estado = "Peso saudável"
+    }else if(imc < 29.99){
+        estado = "Sobrepeso"
+    }else if(imc < 34.99){    
+        estado = "Obesidade (Grau 1)"
+    }else if(imc < 39.99){
+        estado = "Obesidade Severa (Grau 2)"
+    }else if(imc > 40){
+        estado = "Obesidade Mórbida (Grau 3)"    
     }
 
+    } 
 
+    document.getElementById("ficha").innerHTML = "<center>Estado do Paciente</center><br>"+
+    "<br>"
+    "<b>Nome: </b>"+nome+
+    "<br>"
+    "<b>Idade: </b>"+idade+
+    "<br>"
+    "<b>Peso: </b>"+peso+
+    "<br>"
+    "<b>Altura: </b>"+altura+
+    "<br>"
+    "<b>IMC: </b>"+imc+
+    "<br>"
+    "<b>Estado do Paciente: </b>"+estado+
 
+    
+
+{}
 }
