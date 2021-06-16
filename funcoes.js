b1708a2184954c85ab4b8012135e5b8b2d639d0e
Joa1Gomes <joao.gomes181@etec.sp.gov.br>
2021-06-15 22:01:12 -0300

    Finalização do trabalho

--- funcoes.js
+++ funcoes.js
@@ -12,35 +12,27 @@ function operacao(){
         imc = peso/(altura*altura);
 
      if (imc < 18.5){
-        estado = "Peso Baixo"
+        estado = "Peso Baixo";
     }else if(imc < 24.9){
-        estado = "Peso saudável"
+        estado = "Peso saudável";
     }else if(imc < 29.99){
-        estado = "Sobrepeso"
+        estado = "Sobrepeso";
     }else if(imc < 34.99){    
-        estado = "Obesidade (Grau 1)"
+        estado = "Obesidade (Grau 1)";
     }else if(imc < 39.99){
-        estado = "Obesidade Severa (Grau 2)"
+        estado = "Obesidade Severa (Grau 2)";
     }else if(imc > 40){
-        estado = "Obesidade Mórbida (Grau 3)"    
+        estado = "Obesidade Morbida (Grau 3)";
     }
 
-    } 
+    document.getElementById("exibirNome").innerHTML = nome;
+    document.getElementById("exibirIdade").innerHTML = idade;
+    document.getElementById("exibirPeso").innerHTML = peso;
+    document.getElementById("exibirAltura").innerHTML = altura;
+    document.getElementById("exibirEstado").innerHTML = estado;
 
-    document.getElementById("ficha").innerHTML = "<center>Estado do Paciente</center><br>"+
-    "<br>"
-    "<b>Nome: </b>"+nome+
-    "<br>"
-    "<b>Idade: </b>"+idade+
-    "<br>"
-    "<b>Peso: </b>"+peso+
-    "<br>"
-    "<b>Altura: </b>"+altura+
-    "<br>"
-    "<b>IMC: </b>"+imc+
-    "<br>"
-    "<b>Estado do Paciente: </b>"+estado+
-
+    
+  } 
     
 
 {}
